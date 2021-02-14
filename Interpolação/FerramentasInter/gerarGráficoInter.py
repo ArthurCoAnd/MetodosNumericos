6d6ba96cde1f75425a037e595df404e2ae9f6975
Arthur Cordeiro Andrade <arthurcoand@gmail.com>
2021-02-14 17:05:27 -0300

    🔄

--- Interpolação/FerramentasInter/gerarGráficoInter.py
+++ Interpolação/FerramentasInter/gerarGráficoInter.py
@@ -11,7 +11,7 @@ def gerarGráfico(pts,sf):
 
 	x=np.arange(int(min(pts[0])-1),int(max(pts[0])+1),delta)
 	y=np.vectorize(f)
-	pp.title("Gráfico da Função e do Pontos")
+	pp.title("Gráfico da Função e dos Pontos")
 	pp.xlabel("X")
 	pp.ylabel("f(X) & Pontos")
 	pp.plot(x,x-x,'k',x,y(x,sf),'k',pts[0],pts[1],'o')
