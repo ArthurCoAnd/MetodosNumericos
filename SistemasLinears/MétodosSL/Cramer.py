a104e92aec0e0614af80980ebc968c23f7694d02
ArthurCoAnd <arthurcoand@gmail.com>
2021-06-30 14:30:35 -0300

    🔄

--- SistemasLinears/MétodosSL/Cramer.py
+++ SistemasLinears/MétodosSL/Cramer.py
@@ -21,6 +21,10 @@ def Cramer(mat, prec):
 	print("Matriz A")
 	print(str(matA))
 
+	# Determinante de A
+	detA = mm.det(matA)
+	print("Determinante de A = "+str(detA))
+
 	# Matriz B
 	matB = []
 	c = nV
@@ -31,20 +35,43 @@ def Cramer(mat, prec):
 	print(str(matB))
 
 	# Matriz Inversa de A
-	matInvA = matA**-1
-	print("Matriz Inversa A")
-	print(str(matInvA))
+	# matInvA = matA**-1
+	# print("Matriz Inversa A")
+	# print(str(matInvA))
 
 	# Verificar Matriz Inversa A
-	matVer = matA*matInvA
-	print("matriz A * Matriz Inversa A")
-	print(str(matVer))
+	# matVer = matA*matInvA
+	# print("matriz A * Matriz Inversa A")
+	# print(str(matVer))
 
 	# Matriz X
-	matX = matInvA * matB
+	# matX = matInvA * matB
+	# print("Matriz X")
+	# print(str(matX))
+
+	# Matriz X
+	matX = []
+	for n in range(nV):
+		# Matriz An
+		matAn = []
+		for l in range(nV):
+			matAn.append([])
+			for c in range(nV):
+				if c == n:
+					matAn[l].append(matB[l])
+				else:
+					matAn[l].append(matA[l,c])
+		matAn = mm.matrix(matAn)
+		print(f"Matriz A{n}")
+		print(str(matAn))
+		detAn = mm.det(matAn)
+		print(f"Determinante de A{n} = "+str(detAn))
+		xn = mm.mpf(detAn/detA)
+		matX.append(xn)
+	matX = mm.matrix(matX)
 	print("Matriz X")
 	print(str(matX))
-
+		
 	# Erro
 	matE = mm.residual(matA, matX, matB)
 	print("Matriz Erro Residual")
